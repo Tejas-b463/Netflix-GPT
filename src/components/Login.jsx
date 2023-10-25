282fce732debca7bf1bba576c04ec1591cdc90c3
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2023-10-25 18:06:35 +0530

    Build Sign Up Form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './Header'
 
 const Login = () => {
+    const[isSignInForm, setIsSignInForm] = useState()
+    const toggleSignInForm = () => {
+       setIsSignInForm(!isSignInForm);
+    }
   return (
     <div>
     <div><Header/></div>
@@ -9,10 +13,16 @@ const Login = () => {
              <img src="https://assets.nflxext.com/ffe/siteui/vlv3/a73c4363-1dcd-4719-b3b1-3725418fd91d/fe1147dd-78be-44aa-a0e5-2d2994305a13/IN-hi-20231016-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="" />
     </div>
     <form className='bg-opacity-80 bg-black absolute p-12 w-1/2 my-36 mx-auto right-0 left-0 text-white'>
-        <h1 className='font-bold text-3xl py-4'>Sign In</h1>
+        <h1 className='font-bold text-3xl py-4'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+        {isSignInForm && 
+         <input type="text" placeholder='Name' className='p-4 my-4 w-full bg-gray-700'/>
+        }
         <input type="text" placeholder='email Address' className='p-4 my-4 w-full bg-gray-700'/>
         <input type="password" placeholder='Password' className='p-4 my-4 w-full bg-gray-700' />
-        <button className='p-4 my-8 bg-red-700 w-full rounded'>Sign In</button>
+        <button className='p-4 my-8 bg-red-700 w-full rounded'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+        <p className='p-4 my-2 cursor-pointer' onClick={toggleSignInForm}>
+            {isSignInForm ? "New to Netflix? Sign Up Now" : "Already Registered? Sign In Now"}
+        </p>
     </form>
     </div>
   )
